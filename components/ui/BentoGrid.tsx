55b3f4696bef75a5e04f5ffe340a34b7388d5ffa
shamshajili <shamshacili@gmail.com>
2024-12-14 18:58:09 +0400

    bento grid & images & css  code added

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/utils/cn";
+import { BackgroundGradientAnimation } from "./GradientBg";
 
 export const BentoGrid = ({
   className,
@@ -45,16 +46,46 @@ export const BentoGridItem = ({
         className
       )}
       style={{
-        background:'rgb(4,7,29)',
-        backgroundColor:'gradient...'
+        background: 'rgb(2,0,36)',
+        backgroundColor: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)'
       }}
     >
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-          {title}
+
+      <div className={`${id===6}&& ' flex justify-center h-full`}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName,'object-cover,object-center')}
+            />
+          )}
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
+        <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+          {spareImg &&(
+            <img 
+            src={spareImg}
+            alt={spareImg}
+            className={'object-cover,object-center w-full h-full'}
+            />
+          )}
+        </div>
+        <div>
+          {id===6 &&(
+            <BackgroundGradientAnimation>
+              <div className="absolute z-50 flex items-center justify-center text-white font-bold"/>
+            </BackgroundGradientAnimation>
+          )}
+          <div className={cn(
+            titleClassName,'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+          )}>
+            <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
+              {description}
+            </div>
+          <div className="font-sans font-bold text-lg lg:text-2xl max-w-95 z-10">
+            {title}
+          </div>
+          </div>
         </div>
       </div>
     </div>
